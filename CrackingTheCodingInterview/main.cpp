b1edbdd7ebb910cf17eba9d401c71a7a991c2a17
Moch Xiao <cuzv@qq.com>
2015-10-27 21:24:15 +0800

    Initial Commit

---
+++ CrackingTheCodingInterview/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CrackingTheCodingInterview
+//
+//  Created by Moch Xiao on 10/27/15.
+//  Copyright © 2015 Moch Xiao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
